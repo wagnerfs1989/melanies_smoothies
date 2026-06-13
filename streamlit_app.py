6f88187bc59b5ec872b60f0140ea17ec8fd1ff6d
Wagner Ferreira da Silva <wagnerfs1989@hotmail.com>
2026-06-13 18:14:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,6 @@ import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
 st.text(smoothiefroot_response.json())
-sf_df = st.datafrae(data = smoothiefroot_response.json(), use_container_width = True)        
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)        
 
         
