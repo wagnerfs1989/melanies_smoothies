68711828ccaadb945d603c20e6baa01e930e6a26
Wagner Ferreira da Silva <wagnerfs1989@hotmail.com>
2026-06-13 19:19:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ pd_df=my_dataframe.to_pandas()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
-)
+    , max_selections = 5
+    )
 
 if ingredients_list:
     ingredients_string = ''
